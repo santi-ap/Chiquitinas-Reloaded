0971c567c679863012a8838e779ed4f48c590f52
David Alfonso <code.david99@hotmail.com>
2019-06-25 21:37:01 -0600

    espacio para ingresar id y pass opcion aceptar y cancelar(login)

--- ChiquitinasReloaded/src/chiquitinasreloaded/LoginController.java
+++ ChiquitinasReloaded/src/chiquitinasreloaded/LoginController.java
@@ -5,18 +5,54 @@
  */
 package chiquitinasreloaded;
 
+import java.util.Scanner;
+
 /**
  *
  * @author santialfonso
  */
-public class LoginController extends Controller{
+public class LoginController extends Controller {
+
     //metodo que tiene toda la logica para logear un usuario
-    public Usuario logear(){
+    public Usuario logear(int idU, String passU) {
+
+        ServicioUsuario su = new ServicioUsuario();
+        //validar id con base de datos
+        
+        int idBD = 0;//replace 0 with database method query for id
+        String passBD = "0";//replace 0 with database method query for password
+
+        if (idU == idBD) {
+            if(passU)
+
+        }
+
         return null;
     }
-    
+
     //metodo que imprime el menu de login
-    public void menuLogin(){
-        
+    public void menuLogin() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Inserte el ID");
+        int idU = Integer.parseInt(input.nextLine());
+
+        System.out.println("Inserte la contrasenna");
+        String passU = input.nextLine();
+
+        System.out.println("1) Aceptar \n2) Cancelar");
+        String op = input.nextLine();
+
+        switch (op) {
+
+            case "1": //inicia proceso para validar datos de usuario e ingresar
+
+                this.logear(idU, passU);
+                break;
+
+            case "2": //termina proceso de logeo y vuelve al menu anterior
+                break;
+
+        }
     }
 }
