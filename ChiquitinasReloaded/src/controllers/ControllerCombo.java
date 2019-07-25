873b9608606683686329cbc17789e738d5e493a6
Marco <Marco@DESKTOP-BBVUCTE>
2019-07-25 11:03:02 -0600

    Cree un metodo que alimenta en servicioComboHasProducto con el comboId, y el productId para agregar productos al combo.

--- ChiquitinasReloaded/src/controllers/ControllerCombo.java
+++ ChiquitinasReloaded/src/controllers/ControllerCombo.java
@@ -7,6 +7,8 @@ package controllers;
 
 import servicios.Servicio;
 import servicios.ServicioCombo;
+import servicios.ServicioComboHasProducto;
+import java.util.*;
 
 /**
  *
@@ -15,7 +17,8 @@ import servicios.ServicioCombo;
 public class ControllerCombo extends ControllerFactory{
     // ---------------------------------- HAY QUE HACER UN CASTING AQUI PARA PODER IMPLEMENTAR EL PATRON FACTORY
     private ServicioCombo servicioCombo = ((ServicioCombo)this.CrearServicio());//CASTING DE Servcio A ServicioCombo
-
+    private ServicioComboHasProducto servicioComboHasProducto = ((ServicioComboHasProducto)this.CrearServicio());
+    
     public ControllerCombo() {
     }
 
@@ -27,6 +30,14 @@ public class ControllerCombo extends ControllerFactory{
         this.servicioCombo = servicioCombo;
     }
     
+     public ServicioComboHasProducto getServicioComboHasProducto() {
+        return servicioComboHasProducto;
+    }
+
+    public void setServicioComboHasProducto(ServicioComboHasProducto servicioComboHasProducto) {
+        this.servicioComboHasProducto = servicioComboHasProducto;
+    }
+    
     /**
      * METODO NECESARIO PARA IMPLEMENTAR FACTORY
      * @return un nuevo ServicioCombo
@@ -36,7 +47,25 @@ public class ControllerCombo extends ControllerFactory{
         return new ServicioCombo();
     }
     
+    /*
+    @marco
+    Se utiliza el método de insert como un STRING donde el primer valor es el combo ID separa de una coma y el segundo el ID del producto
+    */
     
     
+    public void insertarProductoAUnCombo(){
+    //Lo mejoraré para no estar pidiendo de nuevo el identifier del combo, sino que solamente el del producto
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite el identificador del combo");
+    int primer = sc.nextInt();
+    System.out.println("Digite el identificador del producto");
+    int segundo = sc.nextInt();
+    String conversionIdCombo = String.valueOf(primer);
+    String conversionIdProducto = String.valueOf(segundo);
+    String stringParaMetodo = primer +","+segundo;
+    
+    servicioComboHasProducto.insert(stringParaMetodo);
+    
+    }
     
 }
